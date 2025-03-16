891c3971f9ed85fd5a584142ba4cb9e66f455cf4
kris-2211 <saikris192210@gmail.com>
2025-03-16 20:04:14 +0530

    Bcrypt fixed

--- keeper-backend/models/User.js
+++ keeper-backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   fullname: { type: String, required: true },
@@ -6,6 +7,14 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   avatar: { type: String, default: "default.png" },
   notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Note" }],
+});
+
+ // Hash password before saving
+ UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
